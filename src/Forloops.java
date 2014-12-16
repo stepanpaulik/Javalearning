a627f53b8c56e1b587154f18018aeb3f7579f836
Štěpán Paulík <stepanpaulik@gmail.com>
2014-12-16 11:56:49 +0100

    Loop commit

---
+++ src/Forloops.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Štěpán on 16. 12. 2014.
+ */
+public class Forloops {
+}
--- src/Whileloops.java
+++ src/Whileloops.java
@@ -1,6 +1,11 @@
 
 public class Whileloops {
     public static void main(String[] args) {
+        int value = 0;
 
+        while(value < 10) {
+            System.out.println(value);
+            value++;
+        }
     }
 }
